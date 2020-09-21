b911a6cb1df93193ffac4a2533105bba0c488ccb
miguelrueda <miguel.rueda.e@thalesdigital.io>
2020-09-21 11:51:04 -0500

    Module 8 finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 // Imports for loading & configuring the in-memory web api
-import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
-import { ShellComponent } from "./home/shell.component";
-import { MenuComponent } from "./home/menu.component";
-import { PageNotFoundComponent } from "./home/page-not-found.component";
-import { WelcomeComponent } from "./home/welcome.component";
-import { ProductData } from "./products/product-data";
-import { UserModule } from "./user/user.module";
-import { StoreModule } from "@ngrx/store";
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { ShellComponent } from './home/shell.component';
+import { MenuComponent } from './home/menu.component';
+import { PageNotFoundComponent } from './home/page-not-found.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductData } from './products/product-data';
+import { UserModule } from './user/user.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -29,6 +32,8 @@ import { StoreModule } from "@ngrx/store";
     UserModule,
     AppRoutingModule,
     StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({ name: 'APM dev tools', maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,63 +1,56 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { Subscription } from "rxjs";
-
-import { Product } from "../product";
-import { ProductService } from "../product.service";
-import { Store } from "@ngrx/store";
+import { Product } from '../product';
+import { Store } from '@ngrx/store';
+import { getCurrentProduct, getError, getProducts, getShowProductCode, State } from '../state/product.reducer';
+import * as ProductActions from '../state/product.actions';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: "app-product-list",
-  templateUrl: "./product-list.component.html",
-  styleUrls: ["./product-list.component.css"],
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit, OnDestroy {
-  pageTitle = "Products";
-  errorMessage: string;
+
+  pageTitle = 'Products';
 
   displayCode: boolean;
 
-  products: Product[];
-
-  // Used to highlight the selected product in the list
-  selectedProduct: Product | null;
-  sub: Subscription;
+  products$: Observable<Product[]>;
+  selectedProduct$: Observable<Product>;
+  displayCode$: Observable<boolean>;
+  errorMessage$: Observable<string>;
 
   constructor(
-    private store: Store<any>,
-    private productService: ProductService
-  ) {}
+    private store: Store<State>
+  ) { }
 
   ngOnInit(): void {
-    this.sub = this.productService.selectedProductChanges$.subscribe(
-      (currentProduct) => (this.selectedProduct = currentProduct)
-    );
+    this.products$ = this.store.select(getProducts);
+    this.store.dispatch(ProductActions.loadProducts());
 
-    this.productService.getProducts().subscribe({
-      next: (products: Product[]) => (this.products = products),
-      error: (err) => (this.errorMessage = err),
-    });
+    this.selectedProduct$ = this.store.select(getCurrentProduct);
 
-    this.store.select("products").subscribe((products) => {
-      if (products) {
-        this.displayCode = products.showProductCode;
-      }
-    });
+    this.errorMessage$ = this.store.select(getError);
+
+    this.displayCode$ = this.store.select(getShowProductCode);
   }
 
   ngOnDestroy(): void {
-    this.sub.unsubscribe();
   }
 
   checkChanged(): void {
-    this.store.dispatch({ type: "[Product] Toggle product code" });
+    this.store.dispatch(ProductActions.toggleProductCode());
   }
 
   newProduct(): void {
-    this.productService.changeSelectedProduct(this.productService.newProduct());
+    // this.productService.changeSelectedProduct(this.productService.newProduct());
+    this.store.dispatch(ProductActions.initializeCurrentProduct());
   }
 
   productSelected(product: Product): void {
-    this.productService.changeSelectedProduct(product);
+    // this.productService.changeSelectedProduct(product);
+    this.store.dispatch(ProductActions.setCurrentProduct({ product }));
   }
 }
